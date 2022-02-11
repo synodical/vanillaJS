e9e186858ccd664604f3fe79f1cb114450d08296
synodical <lena0323park@naver.com>
2022-02-11 14:38:49 +0900

    events in JS

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const player = {
-    name: "yeeun",
-    points:10,
-};
-console.log(player.name);
-console.log(player);
+const title = document.querySelector("#title");
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here";
+} 
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeavegit);
+
+//JS로 html element 가져옴
+//가져온 element에 event listener 추가
